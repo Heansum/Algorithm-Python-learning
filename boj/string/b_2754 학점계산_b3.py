ea73658b00ae5710981c77237fc57127ecc6348d
Heansum <heansum1556@gmail.com>
2021-11-09 23:52:15 +0900

    String, b_2754 Credit calculation

---
+++ boj/string/b_2754 학점계산_b3.py
@@ -0,0 +1,27 @@
+_str = input()
+if _str == 'A+':
+    print(4.3)
+elif _str == "A0":
+    print(4.0)
+elif _str == "A-":
+    print(3.7)
+elif _str == "B+":
+    print(3.3)
+elif _str == "B0":
+    print(3.0)
+elif _str == "B-":
+    print(2.7)
+elif _str == "C+":
+    print(2.3)
+elif _str == "C0":
+    print(2.0)
+elif _str == "C-":
+    print(1.7)
+elif _str == "D+":
+    print(1.3)
+elif _str == "D0":
+    print(1.0)
+elif _str == "D-":
+    print(0.7)
+elif _str == "F":
+    print(0.0)
